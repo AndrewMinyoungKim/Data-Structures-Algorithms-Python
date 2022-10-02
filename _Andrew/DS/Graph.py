ac51442fe263f74ab2968d757b7bc5f9755579de
Andrew Kim <akim48415@gmail.com>
2022-10-02 15:36:59 -0400

    adjacency matrix and list

--- _Andrew/DS/Graph.py
+++ _Andrew/DS/Graph.py
@@ -1,5 +1,5 @@
 # Directed Weighted Graph representation using nested dictionaries
-
+# Adjacency List
 graph = {
     'A': {'B': 5, 'C': 4},
     'B': {'D': 2, 'E': 7, 'F': 3},
@@ -12,27 +12,71 @@ graph = {
     'I': {}
 }
 
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Using Nodes:
+# Adjacency List
+
 
 class AdjNode:
-	def __init__(self, data):
-		self.vertex = data
-		self.next = None
+    def __init__(self, data):
+        self.vertex = data
+        self.next = None
+
 
 class Graph:
-	def __init__(self, vertices):
-		self.V = vertices
-		self.graph = [None] * self.V
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [None] * self.V
 
-	# Function to add an edge in an undirected graph
-	def add_edge(self, src, dest):
-		# Adding the node to the source node
-		node = AdjNode(dest)
-		node.next = self.graph[src]
-		self.graph[src] = node
+    # Function to add an edge in an undirected graph
+    def add_edge(self, src, dest):
+        # Adding the node to the source node
+        node = AdjNode(dest)
+        node.next = self.graph[src]
+        self.graph[src] = node
 
-		# Adding the source node to the destination as
-		# it is the undirected graph
-		node = AdjNode(src)
-		node.next = self.graph[dest]
-		self.graph[dest] = node
+        # Adding the source node to the destination as
+        # it is the undirected graph
+        node = AdjNode(src)
+        node.next = self.graph[dest]
+        self.graph[dest] = node
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Adjacency Matrix:
+# Adjacency Matrix representation in Python
+
+
+class Graph(object):
+
+    # Initialize the matrix
+    def __init__(self, size):
+        self.adjMatrix = []
+        for i in range(size):
+            self.adjMatrix.append([0 for i in range(size)])
+        self.size = size
+
+    # Add edges
+    def add_edge(self, v1, v2):
+        if v1 == v2:
+            print("Same vertex %d and %d" % (v1, v2))
+        self.adjMatrix[v1][v2] = 1
+        self.adjMatrix[v2][v1] = 1
+
+    # Remove edges
+    def remove_edge(self, v1, v2):
+        if self.adjMatrix[v1][v2] == 0:
+            print("No edge between %d and %d" % (v1, v2))
+            return
+        self.adjMatrix[v1][v2] = 0
+        self.adjMatrix[v2][v1] = 0
+
+    def __len__(self):
+        return self.size
+
+    # Print the matrix
+    def print_matrix(self):
+        for row in self.adjMatrix:
+            for val in row:
+                print('{:4}'.format(val)),
+            print
